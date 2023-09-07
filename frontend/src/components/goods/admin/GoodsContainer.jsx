c815344e98c9741a7d831f41518789a7e1b9f25b
Nadiia Melnychenko <nadiia092330@gmail.com>
2023-09-07 12:25:29 +0300

    Goods List and filter

--- frontend/src/components/goods/admin/GoodsContainer.jsx
+++ frontend/src/components/goods/admin/GoodsContainer.jsx
@@ -2,32 +2,56 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { responseStatus } from "../../../utils/consts";
 import { Helmet } from "react-helmet-async";
-import { Breadcrumbs, Link, Typography } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import {Breadcrumbs, Link, Pagination, Typography} from "@mui/material";
+import {NavLink, useNavigate, useSearchParams} from "react-router-dom";
+import GoodsList from "./GoodsList";
+import {checkFilterItem, fetchFilterData} from "../../../utils/fetchFilterData";
+import userAuthenticationConfig from "../../../utils/userAuthenticationConfig";
+import GoodsFilter from "./GoodsFilter";
 
 const GoodsContainer = () => {
 
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+
   const [goods, setGoods] = useState(null);
 
+  const [paginationInfo, setPaginationInfo] = useState({
+    totalItems: null,
+    totalPageCount: null,
+    itemsPerPage: 10
+  });
+
+  const [filterData, setFilterData] = useState({
+    "page": checkFilterItem(searchParams, "page", 1, true),
+    "name": checkFilterItem(searchParams, "name", null)
+  });
+
   const fetchProducts = () => {
-    axios.get("/api/products", {
-      "headers": {
-        "Authorization": "Bearer " + localStorage.getItem("token"),
-        "Content-type": "application/json+ld",
-        "Accept": "application/json+ld"
-      }
-    }).then(response => {
+    let filterUrl = fetchFilterData(filterData);
+    navigate(filterUrl);
+
+    axios.get("/api/products" + filterUrl +  "&itemsPerPage=" + paginationInfo.itemsPerPage, userAuthenticationConfig()).then(response => {
       if (response.status === responseStatus.HTTP_OK && response.data["hydra:member"]) {
         setGoods(response.data["hydra:member"]);
+        setPaginationInfo({
+          ...paginationInfo,
+          totalItems: response.data["hydra:totalItems"],
+          totalPageCount: Math.ceil(response.data["hydra:totalItems"] / paginationInfo.itemsPerPage)
+        });
       }
     }).catch(error => {
       console.log("error");
     });
   };
 
+  const onChangePage = (event, page) => {
+    setFilterData({ ...filterData, page: page });
+  };
+
   useEffect(() => {
     fetchProducts();
-  }, []);
+  }, [filterData]);
 
   return (
     <>
@@ -45,13 +69,15 @@ const GoodsContainer = () => {
       <Typography variant="h4" component="h1" mt={1}>
         Goods
       </Typography>
-      <div className="page-style">
-        {goods && goods.map((item, key) => {
-          return <div key={key}>
-            <p>{item.name}</p>
-          </div>;
-        })}
-      </div>
+      <GoodsFilter filterData={filterData} setFilterData={setFilterData}/>
+      <GoodsList goods={goods}/>
+      {paginationInfo.totalPageCount &&
+          <Pagination
+              count={paginationInfo.totalPageCount}
+              shape="rounded"
+              page={filterData.page}
+              onChange={(event, page) => onChangePage(event, page)}
+          />}
     </>
   );
 
